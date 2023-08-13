6ef9b427b174c75aee51784c3c7b90065b9785ed
Developer Awdhesh <awdheshthinker@gmail.com>
2023-08-14 00:00:31 +0530

    changes in user model to have role as well

--- models/User.model.js
+++ models/User.model.js
@@ -6,6 +6,7 @@ const userSchema = mongoose.Schema(
         name:{type:String, required: true},
         email:{type:String, required: true},
         password:{type:String, required: true},
+        role:{type:String, default:"user" }
     }
 )
 const UserModel = mongoose.model("user", userSchema);
