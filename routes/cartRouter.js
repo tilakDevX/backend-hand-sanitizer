bc2f723218bc373fb9b5671076996aadde28de2a
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-02 14:53:01 +0530

    changes

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -15,9 +15,9 @@ CartRouter.post("/", async (req, res) => {
     });
     if (user) {
       //   console.log("this is the user: ", user);
-        await cart_item.save();
+      await cart_item.save();
 
-      res.status(200).send({"Cart item": cart_item});
+      res.status(200).send({ "Cart item": cart_item });
     }
   } catch (error) {
     console.log("Please login first to add product into cart");
@@ -27,36 +27,40 @@ CartRouter.post("/", async (req, res) => {
 });
 
 CartRouter.get("/product", async (req, res) => {
-    try {
-        const cart_items = await CartModel.find({ memberId: req.userId });
+  try {
+    const cart_items = await CartModel.find({ memberId: req.userId });
 
-        if (!cart_items || cart_items.length === 0) {
-            return res.status(200).json({ "message": "cart is empty" });
-        }
-        
-        res.status(200).json(cart_items);
-    } catch (error) {
-        console.error("Error while getting products from cart", error);
-        res.status(500).send("Error while getting products from cart");
+    if (!cart_items || cart_items.length === 0) {
+      return res.status(200).json({ message: "cart is empty" });
     }
+
+    res.status(200).json(cart_items);
+  } catch (error) {
+    console.error("Error while getting products from cart", error);
+    res.status(500).send("Error while getting products from cart");
+  }
 });
 
 CartRouter.delete("/product/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const deletedItem = await CartModel.findOneAndDelete({ memberId: req.userId, _id: id });
+    const deletedItem = await CartModel.findOneAndDelete({
+      memberId: req.userId,
+      _id: id,
+    });
 
-        if (!deletedItem) {
-            return res.status(404).json({ message: "Item not found in the cart" });
-        }
-
-        res.status(200).json({ message: "Item deleted successfully" });
-    } catch (error) {
-        console.error("Error while deleting products from cart", error);
-        res.status(500).json({ message: "Error while deleting products from cart" });
+    if (!deletedItem) {
+      return res.status(404).json({ message: "Item not found in the cart" });
     }
-});
 
+    res.status(200).json({ message: "Item deleted successfully" });
+  } catch (error) {
+    console.error("Error while deleting products from cart", error);
+    res
+      .status(500)
+      .json({ message: "Error while deleting products from cart" });
+  }
+});
 
 module.exports = { CartRouter };
