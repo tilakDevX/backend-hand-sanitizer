71f9c5a786355e25385c16df220b3b630aabd564
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-02 14:51:06 +0530

    cart get method impleted

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -26,6 +26,21 @@ CartRouter.post("/", async (req, res) => {
   }
 });
 
+CartRouter.get("/product", async (req, res) => {
+    try {
+        const cart_items = await CartModel.find({ memberId: req.userId });
+
+        if (!cart_items || cart_items.length === 0) {
+            return res.status(200).json({ "message": "cart is empty" });
+        }
+        
+        res.status(200).json(cart_items);
+    } catch (error) {
+        console.error("Error while getting products from cart", error);
+        res.status(500).send("Error while getting products from cart");
+    }
+});
+
 
 
 
