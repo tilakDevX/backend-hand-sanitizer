a39eb96d4370452a84e43bc2477c5a49fc51171c
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-02 14:54:03 +0530

    basic modification

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,9 +4,6 @@ const { CartModel } = require("../models/Cart.model");
 
 const CartRouter = Router();
 
-
-
-
 CartRouter.get("/product", async (req, res) => {
   try {
     const cart_items = await CartModel.find({ memberId: req.userId });
@@ -23,26 +20,26 @@ CartRouter.get("/product", async (req, res) => {
 });
 
 CartRouter.post("/", async (req, res) => {
-    try {
-      console.log(req.body);
-  
-      const user = await UserModel.findById(req.userId);
-      const cart_item = new CartModel({
-        memberId: req.userId,
-        product: req.body,
-      });
-      if (user) {
-        //   console.log("this is the user: ", user);
-        await cart_item.save();
-  
-        res.status(200).send({ "Cart item": cart_item });
-      }
-    } catch (error) {
-      console.log("Please login first to add product into cart");
-  
-      res.status(404).send("Please login first to add product into cart");
+  try {
+    console.log(req.body);
+
+    const user = await UserModel.findById(req.userId);
+    const cart_item = new CartModel({
+      memberId: req.userId,
+      product: req.body,
+    });
+    if (user) {
+      //   console.log("this is the user: ", user);
+      await cart_item.save();
+
+      res.status(200).send({ "Cart item": cart_item });
     }
-  });
+  } catch (error) {
+    console.log("Please login first to add product into cart");
+
+    res.status(404).send("Please login first to add product into cart");
+  }
+});
 
 CartRouter.delete("/product/:id", async (req, res) => {
   try {
